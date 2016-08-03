0f1cd1bc93ea64d1db89478b1dac30c8f5debefc
speedingdeer <ffwisniewski@gmail.com>
2016-08-03 14:50:49 +0200

    Created 2016 build.

---
+++ 2016/js/assets.js
@@ -0,0 +1,12 @@
+(function() {
+  var root;
+
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
+
+  $(function() {
+    return root.assets = {
+      audienceGif: "/2016//img/audience.gif"
+    };
+  });
+
+}).call(this);
